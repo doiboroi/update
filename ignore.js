1921bd0ea58faa6b039743c36381cd41bade57d8
doiboroi <doiboroi@users.noreply.github.com>
2023-03-13 14:29:56 +0700

    Update ignore.js

--- ignore.js
+++ ignore.js
@@ -1,3 +1,3 @@
-var sopi = "sopi";
-var iIgnoreV = 3.101;
+var sopi = "sop2";
+var iIgnoreV = 3.102;
 var aIgnorePermanent = ["shop_duadon","tothithuthuy2019","qcataa7040","2092rxsddb","tbmjqj8872","dwzgsfn16071","zyytjrq10414","fdkksmc16144","rzrwmeg11458","yhmeucq13292","qewajfg16391", "phanxuong1945", "m3whm_ywtg","bestbuyvietnam.vn","shopthecao499","hongcam3701", "linh13022002", "shopeg1992", "ninshoes_vnxk", "x.store.thoitrang.hottrend", "kelna26", "loix.shop","banana_like","awifi","thanho950", "thunguyen0511", "vietluyen1990", "q3an5daija", "boisan.08", "_t_h_a_i___t_b_", "ngocanhtruong", "hilumoni"];
