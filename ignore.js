c65d328e4071f7e6f19609b63e166ac3ab3a2450
doiboroi <doiboroi@users.noreply.github.com>
2023-03-25 20:12:58 +0700

    Update ignore.js

--- ignore.js
+++ ignore.js
@@ -4,4 +4,4 @@ var aIgnorePermanent = ["shop_duadon","tothithuthuy2019","qcataa7040","2092rxsdd
 
 
 var sopi = "sopi";
-var aPriorityUS = ["kimdungjn", "nerman.vn"];
+var aPriorityUS = ["kimdungjn"];
