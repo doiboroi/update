dbbd7d3ff171d41f399bab7338fb791e487e2d3e
doiboroi <doiboroi@users.noreply.github.com>
2023-08-17 16:51:10 +0700

    Update ignore.js

--- ignore.js
+++ ignore.js
@@ -1,6 +1,6 @@
-var iIgnoreV = 6;
-var aIgnorePermanent = ["shop_duadon","tothithuthuy2019","qcataa7040","2092rxsddb","tbmjqj8872","dwzgsfn16071","zyytjrq10414","fdkksmc16144","rzrwmeg11458","yhmeucq13292","qewajfg16391", "phanxuong1945", "m3whm_ywtg","bestbuyvietnam.vn","shopthecao499","hongcam3701", "linh13022002", "shopeg1992", "ninshoes_vnxk", "x.store.thoitrang.hottrend", "kelna26", "loix.shop","banana_like","awifi","thanho950", "thunguyen0511", "vietluyen1990", "q3an5daija", "boisan.08", "_t_h_a_i___t_b_", "ngocanhtruong", "hilumoni", "trung_store99", "annhubedding","trendytops", "yenvy_1010","thegioituixachqc","dwinofficial","sanxinh.store","bumbumfamily", "big.c.nha.eg", "mc21.studios", "beo_xinh_shop", "decaoofficial", "nailxinh.ling", "chuyen.tour.trung.quoc", "btcoffee", "lissahuynhjfe462", "hoanboymen", "sen12345.", "my622008", "vimeinh2606","cnginh160","cuong96699999"];
+var iIgnoreV = 7;
+var aIgnorePermanent = ["shop_duadon","tothithuthuy2019","qcataa7040","2092rxsddb","tbmjqj8872","dwzgsfn16071","zyytjrq10414","fdkksmc16144","rzrwmeg11458","yhmeucq13292","qewajfg16391", "phanxuong1945", "m3whm_ywtg","bestbuyvietnam.vn","shopthecao499","hongcam3701", "linh13022002", "shopeg1992", "ninshoes_vnxk", "x.store.thoitrang.hottrend", "kelna26", "loix.shop","banana_like","awifi","thanho950", "thunguyen0511", "vietluyen1990", "q3an5daija", "boisan.08", "_t_h_a_i___t_b_", "ngocanhtruong", "hilumoni", "trung_store99", "annhubedding","trendytops", "yenvy_1010","thegioituixachqc","dwinofficial","sanxinh.store","bumbumfamily", "big.c.nha.eg", "mc21.studios", "beo_xinh_shop", "decaoofficial", "nailxinh.ling", "chuyen.tour.trung.quoc", "btcoffee", "lissahuynhjfe462", "hoanboymen", "sen12345.", "my622008", "vimeinh2606","cnginh160","cuong96699999","hithnglnng"];
 
 
 var sopi = "sopi";
-var aPriorityUS = ["kimdungjn"];
+var aPriorityUS = ["maybedding.com.vn"];
